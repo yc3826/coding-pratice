71236b184ad82aca30bb5ef9e9cda638ee672757
yc3826 <77368106+yc3826@users.noreply.github.com>
2021-03-17 00:03:08 +0800

    week7

--- week7/script.js
+++ week7/script.js
@@ -1,4 +1,22 @@
-console.log("hi")
+// console.log("hi")
 
-let myElement=document.getElementById("speical")
-console.log(myElement)
+// let myElement=document.getElementById("speical")
+// console.log(myElement)
+
+/** Write your code below each comment */
+
+/* Get the element with an ID of 'first' */
+var first = document.getElementById("first");
+// console.log(first);
+
+/* Now delete that first element (in the html file), and re-run your code. What changed? */
+
+/* Get the elements with a class of 'second' with getElementsByClassName: */
+var second = document.getElementsByClassName("second");
+console.log(second);
+/* Delete one of the elements with a class of 'second' and refresh the page. Do you get a similar result? */
+
+/* Get the span element using getElementsByTagname: */
+var third=document.getElementsByTagName("span")
+console.log(third)
+/* Add a span anywhere, and re-run the last piece of code. What's different? */
