1c03f4adc0738a1924ca96258e0b3214d9c9d3cd
yc3826 <77368106+yc3826@users.noreply.github.com>
2021-04-20 07:33:52 +0800

    project_04

---
+++ project_04/script.js
@@ -0,0 +1,32 @@
+
+// var zoom = 1;
+// var zoomStep = 0.2;
+// document.getElementById("Out").addEventListener("click",function(){
+//     if(zoom > zoomStep){zoom -= zoomStep; document.getElementById("PART2").style.transform = "scale("+zoom+")";}});
+
+
+
+
+    function setZoom(zoom,el) {
+      
+        transformOrigin = [0,1];
+          el = el || instance.getpart2();
+          var p = ["webkit", "moz", "ms", "o"],
+              s = "scale(" + zoom + ")",
+              oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
+  
+          for (var i = 0; i < p.length; i++) {
+              el.style[p[i] + "Transform"] = s;
+              el.style[p[i] + "TransformOrigin"] = oString;
+          }
+  
+          el.style["transform"] = s;
+          el.style["transformOrigin"] = oString;
+        
+  }
+  
+
+  function showVal(a){
+     var zoomScale = Number(a)/10;
+     setZoom(zoomScale,document.getElementsByClassName('part2')[0])
+  }
